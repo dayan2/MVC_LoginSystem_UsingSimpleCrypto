4f1ff3468fe511b890179f93b3cedfca56e01193
dayan2 <dayanmendis2@gmail.com>
2015-01-03 17:27:46 +0530

    LoginPage_CryptedPassword
    you need to install the SimpleCrypto nuget from da visual studio

---
+++ Mvc4_Login/Mvc4_Login/Controllers/UserController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Mvc4_Login.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.User user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (IsValid(user.Email, user.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(user.Email, false);
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                    ModelState.AddModelError("","Login details are incorrect");
+            }
+            return View(user);
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Models.User user)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var con = new MainDBContext())
+                {
+                    var crypto = new SimpleCrypto.PBKDF2();
+                    var pwd = crypto.Compute(user.Password);
+                    var sysuser = con.Users.Create();
+
+                    sysuser.Email = user.Email;
+                    sysuser.Password = pwd;
+                    sysuser.PasswordSalt = crypto.Salt;
+                    sysuser.Id = Guid.NewGuid();
+
+                    con.Users.Add(sysuser);
+                    con.SaveChanges();
+
+                    return RedirectToAction("Index","Home");
+                }
+            }
+            return View(user);
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index","User");
+        }
+
+        private bool IsValid(string email, string pwd)
+        {
+            var cryto = new SimpleCrypto.PBKDF2();
+            bool isvalid = false;
+
+            using (var con = new MainDBContext())
+            {
+                User user = con.Users.FirstOrDefault(e => e.Email == email);
+
+                if (user != null)
+                {
+                    if (user.Password == cryto.Compute(pwd, user.PasswordSalt))
+                        isvalid = true;
+                }
+            }
+
+            return isvalid;
+        }
+    }
+}
---
+++ Mvc4_Login/Mvc4_Login/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mvc4_Login.Models
+{
+    public class User
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(150)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
